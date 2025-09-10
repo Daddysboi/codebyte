3c67593fa586f6597014f68766253b749216d394
Tems <ayusuf@byteworks.com.ng>
2025-09-11 00:31:37 +0100

    Clean up

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,111 +1,31 @@
-import styled from "styled-components";
-
+import React from "react"; // Import React
 import { FaExternalLinkAlt } from "react-icons/fa";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column;
-  border-radius: 0.5rem;
-  box-shadow: 1px 1px 2px 2px rgba(0.2, 0.2, 0.2, 0.07);
-  padding-top: 0.5rem;
-  width: 18rem;
-  /* height: 26rem; */
-  border: 2px solid #181f33;
-  text-align: left;
-  font-family: inter, serif;
-  color: #a0a6b6;
-`;
-
-const Top = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  /* border-bottom: 2px solid #181f33; */
-  height: 10rem;
-`;
-
-const Img = styled.img`
-  height: 10rem;
-  padding: 0.5rem;
-`;
-
-const Header = styled.p`
-  font-size: 01rem;
-  margin: 0.02rem;
-  font-weight: 500;
-  padding-bottom: 1rem;
-  text-align: center;
-  color: #fff;
-`;
-
-const Paragraph = styled.p`
-  font-size: 0.7rem;
-  padding-bottom: 1rem;
-`;
-
-const Ul = styled.ul`
-  list-style-type: none;
-`;
-
-const Key = styled.p`
-  color: #fff;
-  display: inline;
-  padding-right: 0.3rem;
-`;
-const Li = styled.li`
-  text-decoration: none;
-  font-size: 0.65rem;
-  text-align: start;
-  /* padding-left: 1rem; */
-`;
-
-const Bottom = styled.div`
-  /* display: flex; */
-  flex-direction: column;
-  justify-content: space-between;
-  /* height: 40%; */
-  align-items: center;
-  padding: 0.3rem 1rem 0.5rem 1rem;
-`;
-
-const ClickText = styled.p`
-  text-align: right;
-  padding-right: 1rem;
-  color: #efcc18;
-  &:hover {
-    color: white;
-  }
-  &:visited {
-    color: gray;
-  }
-`;
 
 const ProfileCard = ({ img, href, name, tools, text }) => {
   return (
-    <Container>
-      <Top>
-        <Img src={img} alt="userimage" />
-      </Top>
-      <Bottom>
-        <Header>{name}</Header>
-        <Paragraph>{text}</Paragraph>
-        <Ul>
+    <div className="profile-card-container">
+      <div className="profile-card-top">
+        <img className="profile-card-img" src={img} alt="userimage" />
+      </div>
+      <div className="profile-card-bottom">
+        <p className="profile-card-header">{name}</p>
+        <p className="profile-card-paragraph">{text}</p>
+        <ul className="profile-card-ul">
           {tools.map((tool, index) => (
-            <Li key={index}>
-              <Key>{tool.key}: </Key>
+            <li key={index} className="profile-card-li">
+              <p className="profile-card-key">{tool.key}: </p>
               {tool.value.filter(Boolean).join(", ")}
-            </Li>
+            </li>
           ))}
-        </Ul>
+        </ul>
 
         <a href={href} rel="noopener noreferrer" target="_blank">
-          <ClickText>
+          <p className="profile-card-click-text">
             <FaExternalLinkAlt size={"0.8rem"} />
-          </ClickText>
+          </p>
         </a>
-      </Bottom>
-    </Container>
+      </div>
+    </div>
   );
 };
 
