df3155f186fc97468520446a53e1d6e1b23f4aa4
Tems <ayusuf@byteworks.com.ng>
2025-09-14 01:06:12 +0100

    Style Fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,4 @@
 import React, { useEffect, Suspense, lazy } from "react";
-import Aos from "aos";
-import "aos/dist/aos.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -14,11 +12,6 @@ import Contact from "../components/contact";
 import Loading from "../components/Loading";
 
 function Home({ isDarkMode }) {
-  useEffect(() => {
-    Aos.init({
-      duration: 2000,
-    });
-  }, []);
 
   return (
     <>
